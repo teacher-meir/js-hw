cdc51b5dce72ff02509a9e3580be76b1bdc2aad0
teacher-meir <aditeachermeir@gmail.com>
2025-12-14 05:31:11 +0200

    add style like chess - by js no css

--- 1-mult-board/script.js
+++ 1-mult-board/script.js
@@ -4,7 +4,8 @@ let board = `<table dir="ltr"><tbody>`;
 for (let i = 1; i <= size; i++) {
     board += `<tr>`;
     for (let j = 1; j <= size; j++) {
-        board += `<td>${i * j}</td>`;
+        const color = ((i + j) % 2 === 0) ? "#cfe2f3" : "#f6b5d8";
+        board += `<td style="background-color: ${color}">${i * j}</td>`;
     }
     board += `</tr>`;
 }
