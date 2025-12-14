997132e687689d4dda1bf97223fa39d17935f6cf
teacher-meir <aditeachermeir@gmail.com>
2025-12-14 05:36:05 +0200

    add headers - row & columns

--- 1-mult-board/script.js
+++ 1-mult-board/script.js
@@ -1,8 +1,16 @@
 const size = parseInt(prompt("הקש גודל לוח", 10));
 
 let board = `<table dir="ltr"><tbody>`;
+
+// שורת כותרת
+board += `<tr><th>&times</th>`;
+for (let j = 1; j <= size; j++) {
+    board += `<th>${j}</th>`;
+}
+board += `</tr>`;
+
 for (let i = 1; i <= size; i++) {
-    board += `<tr>`;
+    board += `<tr><th>${i}</th>`;
     for (let j = 1; j <= size; j++) {
         const color = ((i + j) % 2 === 0) ? "#cfe2f3" : "#f6b5d8";
         board += `<td style="background-color: ${color}">${i * j}</td>`;
