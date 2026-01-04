2fead51a2b7ceb90bba59afd1972ff41bacb4c6f
teacher-meir <aditeachermeir@gmail.com>
2026-01-04 23:54:05 +0200

    feat 1: enhance triangle drawing to accept color input and update styling

--- 3-math/script.js
+++ 3-math/script.js
@@ -1,19 +1,20 @@
 function drawTriangleInput() {
     const size = +prompt('enter triangle size!');
+    const color = prompt('enter triangle color!');
 
     if (isNaN(size) || size <= 0) {
         alert('גודל צלע לא תקין')
     } else {
-        drawTriangle(size);
+        drawTriangle(size, color);
     }
 }
-function drawTriangle(size) {
+function drawTriangle(size, color) {
     const yeter = Math.sqrt(size ** 2 + size ** 2);
     const perimeter = 2 * size + yeter;
 
     document.body.innerHTML += `
     <div class="triangle"
-        style="border-width: ${size}px;"
+        style="border-width: ${size}px; border-color: transparent transparent ${color} ${color};"
         onclick="alert('היקף המשולש הוא ${perimeter}')"></div>
     `;
 }
