9a73bd5516e12cca140a7adf0366b8be37daf228
teacher-meir <aditeachermeir@gmail.com>
2026-01-04 20:42:03 +0200

    add empty functions to script

---
+++ 3-math/script.js
@@ -0,0 +1,12 @@
+function drawTriangleInput() { }
+function drawTriangle(size) { }
+
+function drawSquareInput() { }
+function drawSquare(size) { }
+
+function randomImage() { }
+
+function calcBMIInput() { }
+function calcBMI(height, weight) { }
+
+function testYourself() { }
