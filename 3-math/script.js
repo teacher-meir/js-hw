a8b4667c311110ff37075b67211f7c46bb1550a7
teacher-meir <aditeachermeir@gmail.com>
2026-01-04 20:44:07 +0200

    add triangle function

--- 3-math/script.js
+++ 3-math/script.js
@@ -1,5 +1,10 @@
-function drawTriangleInput() { }
-function drawTriangle(size) { }
+function drawTriangleInput() {
+    const size = +prompt('enter triangle size!');
+    drawTriangle(size);
+}
+function drawTriangle(size) {
+    document.body.innerHTML += `<div class="triangle" style="border-width: ${size}px;"></div>`;
+}
 
 function drawSquareInput() { }
 function drawSquare(size) { }
